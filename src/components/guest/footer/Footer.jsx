1d1bcf338a2f75dd0345eef2e67de664ded3fb9a
Mathieu Etourneaud <m.etourneaud@gmail.com>
2024-05-14 16:04:58 +0200

    Add social media images and reorganization of footer

--- src/components/guest/footer/Footer.jsx
+++ src/components/guest/footer/Footer.jsx
@@ -1,29 +1,73 @@
 import React from "react"
 import "./Footer.scss"
+import { Link } from "react-router-dom"
 
 const Footer = () => {
-  const scrollToTop = () => {
-    window.scrollTo({
-      top: 0,
-      behavior: "smooth",
-    })
-  }
+  // const scrollToTop = () => {
+  //   window.scrollTo({
+  //     top: 0,
+  //     behavior: "smooth",
+  //   })
+  // }
 
   return (
     <footer className="foot">
-      <ul className="ulFooter">
-        <li className="liFooter">
-          <h2 className="liArchives">Les Archives d'Ohara</h2>
-        </li>
-        <li className="liTop">
-          <img
-            className="topButton"
-            src="/assets/images/topButton.svg"
-            alt="topButton"
-            onClick={scrollToTop}
-          />
-        </li>
-      </ul>
+      <div className="logoContainer">
+        <Link to="/">
+          <img className="logoFooter" src="/assets/images/logo.png" alt="logo" />
+        </Link>
+      </div>
+      <nav>
+        <ul className="ulFooter">
+          <li className="liCgu">
+            <Link className="hover-foot" to="/contact">
+              CGU
+            </Link>
+          </li>
+          <li className="liFooter">
+            <Link to="https://twitter.com">
+              <img
+                className="logoSocialMedia"
+                src="/assets/images/socialMedia/twitter.png"
+                alt="logo twitter"
+              />
+            </Link>
+          </li>
+          <li className="liFooter">
+            <Link to="https://www.facebook.com/">
+              <img
+                className="logoSocialMedia"
+                src="/assets/images/socialMedia/facebook.png"
+                alt="logo facebook"
+              />
+            </Link>
+          </li>
+          <li className="liFooter">
+            <Link to="https://www.instagram.com/">
+              <img
+                className="logoSocialMedia"
+                src="/assets/images/socialMedia/instagram.png"
+                alt="logo instagram"
+              />
+            </Link>
+          </li>
+          <li className="liFooter liYoutube">
+            <Link to="https://www.youtube.com">
+              <img
+                className="logoSocialMedia"
+                src="/assets/images/socialMedia/youtube.png"
+                alt="logo youtube"
+              />
+            </Link>
+          </li>
+          <li className="liFooter">
+            <Link className="hover-foot" to="/contact">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+      <h2 className="copyright">&copy;Les Archives d'Ohara</h2>
     </footer>
   )
 }
--- src/components/guest/header/Header.jsx
+++ src/components/guest/header/Header.jsx
@@ -54,7 +54,7 @@ const Header = () => {
   }
 
   return (
-    <header className="header-container">
+    <header>
       <nav>
         <ul className="ulNav">
           <li className="liLogo">
