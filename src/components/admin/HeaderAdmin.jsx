6531766d61db94af64c089cf9e715490e407dfae
Mathieu Etourneaud <m.etourneaud@gmail.com>
2024-06-05 16:29:43 +0200

    Add menu burger to admin header

--- src/components/admin/HeaderAdmin.jsx
+++ src/components/admin/HeaderAdmin.jsx
@@ -5,6 +5,7 @@ import "./HeaderAdmin.scss"
 const HeaderAdmin = () => {
   const navigate = useNavigate()
   const [isAuthenticated, setIsAuthenticated] = useState(localStorage.getItem("jwt") !== null)
+  const [showLinks, setShowLinks] = useState(false)
 
   const handleLogout = () => {
     console.log("Déconnexion effectuée") // Message de débogage
@@ -21,36 +22,43 @@ const HeaderAdmin = () => {
     }
   }, [isAuthenticated, navigate])
 
+  const handleShowLinks = () => {
+    setShowLinks(!showLinks)
+  }
+
   return (
     <header>
-      <nav>
-        <ul className="ulNav">
-          <li className="liLogo">
-            <Link to="/home">
-              <img className="logo" src="/assets/images/logo.png" alt="logo"></img>
-            </Link>
-          </li>
-          <li className="liNav">
-            <Link className="hover-link" to="/admin/mangas">
+      <nav className={`navbar ${showLinks ? "show-nav" : "hide-nav"}`}>
+        <div className="liLogo navbar_logo">
+          <Link to="/home">
+            <img className="logo" src="/assets/images/logo.png" alt="logo"></img>
+          </Link>
+        </div>
+        <ul className="navbar_links">
+          <li className="navbar_item slideInDown-1">
+            <Link className="hover-link navbar_link" to="/admin/mangas">
               Gestion des mangas
             </Link>
           </li>
-          <li className="liNav">
-            <Link className="hover-link" to="/admin/mangas/create">
+          <li className="navbar_item slideInDown-1">
+            <Link className="hover-link navbar_link" to="/admin/mangas/create">
               Création d'un manga
             </Link>
           </li>
-          <li className="liNav liSpace">
-            <Link className="hover-link" to="/admin/users">
+          <li className="navbar_item slideInDown-1">
+            <Link className="hover-link navbar_link" to="/admin/users">
               Gestion des utilisateurs
             </Link>
           </li>
-          <li className="liNav">
-            <Link className="hover-link" onClick={handleLogout}>
+          <li className="navbar_item slideInDown-1">
+            <Link className="hover-link navbar_link" onClick={handleLogout}>
               Se déconnecter
             </Link>
           </li>
         </ul>
+        <button className="navbar_burger" onClick={handleShowLinks}>
+          <span className="burger-bar"></span>
+        </button>
       </nav>
     </header>
   )
